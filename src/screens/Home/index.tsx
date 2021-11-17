5406ccdcc4ec47ab3bf80e942d6696643836daca
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-11-17 18:50:00 -0300

    Criando a lista de categorias

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { View } from 'react-native';
-import { ButtonAdd } from '../../components/ButtonAdd';
 
+import { CategorySelect } from '../../components/CategorySelect';
+import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
 
 import { styles } from './styles';
@@ -10,8 +11,10 @@ export function Home() {
     return (
         <View>
             <View style={styles.header}>
-                <Profile />
-                <ButtonAdd />
+                
+                <View>
+                    <CategorySelect />
+                </View>
             
             </View>
         </View>
