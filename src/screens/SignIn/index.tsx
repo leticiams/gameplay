77cb4aa9ca3389792b77523660090229d97a0d15
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-11-17 10:19:14 -0300

    Adicionando fontes personalizadas

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, StatusBar } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
@@ -9,15 +9,13 @@ export function SignIn() {
     return (
         <View style={styles.container}>
             
-            <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-
             <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
 
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize {`\n`}
-                    suas jogatinas {`\n`}
-                    facilmente
+                    Conecte-se {`\n`}
+                    e orgnize suas {`\n`}
+                    jogatinas
                 </Text>
                 <Text style={styles.subtitle}>
                     Crie grupos para jogar seus games {`\n`}
