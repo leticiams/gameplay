df321439550932d3dc681bdf17dbf82e10940ac0
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-11-17 08:14:10 -0300

    Adicionando e estilizando tela de Sign In

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -10,7 +10,19 @@ export function SignIn() {
     return (
         <View style={styles.container}>
             
-            <Image source={IllustrationImg} />
+            <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
+
+            <View style={styles.content}>
+                <Text style={styles.title}>
+                    Organize {`\n`}
+                    suas jogatinas {`\n`}
+                    facilmente
+                </Text>
+                <Text style={styles.subtitle}>
+                    Crie grupos para jogar seus games {`\n`}
+                    favoritos com seus amigos
+                </Text>
+            </View>
 
         </View>
     );
