091f91d3127c06cb9a6b1a78554db7a28be6fabb
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-11-17 08:28:48 -0300

    Criando o botão de acesso ao discord

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, Text, Image, StatusBar } from 'react-native';
 
+import { ButtonIcon } from '../../components/ButtonIcon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
@@ -22,6 +23,8 @@ export function SignIn() {
                     Crie grupos para jogar seus games {`\n`}
                     favoritos com seus amigos
                 </Text>
+
+                <ButtonIcon />
             </View>
 
         </View>
