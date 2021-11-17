8477e49445ef1a4aa5033e2c590d52a8b488d1ea
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-11-17 08:16:15 -0300

    Estilizando a barra de status

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Image, StatusBar } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 export function SignIn() {
-    const [text, setText] = useState('');
-
     return (
         <View style={styles.container}>
             
+            <StatusBar barStyle="light-content" />
+
             <Image source={IllustrationImg} style={styles.image} resizeMode="stretch" />
 
             <View style={styles.content}>
