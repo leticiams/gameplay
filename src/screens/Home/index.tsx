17a048f5321d0a7b1a4bbfbe953b72899372a7cf
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-11-24 09:14:03 -0300

    Criando componente de lista

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -27,6 +27,10 @@ export function Home() {
                       setCategory={handleCategorySelect}
                     />
                 </View>
+
+                <View style={styles.content}>
+
+                </View>
         </View>
     );
 }
