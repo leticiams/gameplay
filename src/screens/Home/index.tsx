dcb1b5dbb0cb4f42383dd6032a7d2ec7d9874afa
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-11-24 09:29:03 -0300

    Adicionando o componente na Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,30 @@
 import React, { useState } from 'react';
-import { View } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 
 import { CategorySelect } from '../../components/CategorySelect';
 import { ButtonAdd } from '../../components/ButtonAdd';
 import { Profile } from '../../components/Profile';
+import { ListHeader  } from '../../components/ListHeader';
 
 import { styles } from './styles';
 
 export function Home() {
     const [category, setCategory] = useState('');
+    
+    const appointments = [
+        {
+            id: '1',
+            guild: {
+                id: '1',
+                name: 'Lendários',
+                icon: null,
+                owner: true
+            },
+            category: '1',
+            date: '30/11 - 20:40h',
+            description: 'É hoje que vamos chegar ao challenger sem perder uma partida da md10!' 
+        }
+    ]
 
     function handleCategorySelect(categoryId: string) {
         categoryId === category ? setCategory('') : setCategory(categoryId);
@@ -23,12 +39,24 @@ export function Home() {
                 
                 <View>
                     <CategorySelect 
-                      categorySelected={category}
+                      categorySelected={category} 
                       setCategory={handleCategorySelect}
                     />
                 </View>
 
                 <View style={styles.content}>
+                    <ListHeader 
+                        title="Partidas agendadas"
+                        subtitle="Total 6"
+                    />
+
+                    <FlatList 
+                        data={appointments}
+                        keyExtractor={item => item.id}
+                        renderItem={({ item }) => (
+                            <Text>{item.guild.name}</Text>
+                        )}
+                    />
 
                 </View>
         </View>
