677a3a5f173eb60d313e1502cd383eb2dec2e5a5
leticia.souza <leticia.souza@Leticias-MacBook-Pro.local>
2021-12-07 21:20:35 -0300

    Criando tela de Jogadores

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
-import { View } from 'react-native';
-import { ImageBackground } from 'react-native';
+import { View, Text, ImageBackground, FlatList } from 'react-native';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 import BannerImg from '../../assets/banner.png';
 
+
+import { ListHeader } from '../../components/ListHeader';
+import { ListDivider } from '../../components/ListDivider';
 import { Background } from '../../components/Background';
+import { ButtonIcon } from '../../components/ButtonIcon';
 import { Header } from '../../components/Header';
+import { Member } from '../../components/Member';
 
 export function AppointmentDetails() {
+    const members = [
+        {
+            id: '1',
+            username: 'Lucas Calebe',
+            avatar_url: 'https://github.com/lucascalebe.png',
+            status: 'online'
+        },
+        {
+            id: '2',
+            username: 'Calebe',
+            avatar_url: 'https://github.com/lucascalebe.png',
+            status: 'offline'
+        }
+    ]
+
     return (
         <Background>
             <Header 
@@ -31,9 +50,36 @@ export function AppointmentDetails() {
                 source={BannerImg}
                 style={styles.banner}
             >
+                <View style={styles.bannerContent}>
+                    <Text style={styles.title}>
+                        Lendários
+                    </Text>
 
+                    <Text style={styles.subtitle}>
+                        É hoje que vamos chegar ao challenger sem perder uma md10!
+                    </Text>
+                </View>
             </ImageBackground>
 
+            <ListHeader 
+                title="Jogadores"
+                subtitle="Total 3"
+            />
+
+            <FlatList 
+                data={members}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => (
+                    <Member data={item} />
+                )}
+                ItemSeparatorComponent={() => <ListDivider />}
+                style={styles.members}
+            />
+
+            <View style={styles.footer}>
+                <ButtonIcon title="Entra na partida" /> 
+            </View>
+            
         </Background>
     );
 }
